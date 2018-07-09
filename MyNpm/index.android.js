190efddba7e2807173abcafb4d2b9e6ce95eed39
aimeituan <aimeituan@meituan.com>
2018-07-09 13:24:22 +0800

    state例子实战；
    Signed-off-by: aimeituan <aimeituan@meituan.com>

--- MyNpm/index.android.js
+++ MyNpm/index.android.js
@@ -2,6 +2,51 @@
 import React, {Component} from 'react'; //导入React的依赖
 import {AppRegistry, StyleSheet, Text, View} from 'react-native'; //从react-native包中导入我们需要用的控件，这些控件都是fb官方帮我们在js端封装过的，对应的在native端也已经按照规则暴露并注册过了即在2.4的getPackages()中我们z注册的MainReactPackage
 
+
+/**
+ * state实践例子
+ */
+class Blink extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { showText: true };
+
+        // 每1000毫秒对showText状态做一次取反操作
+        setInterval(() => {
+            this.setState(previousState => {
+                return { showText: !previousState.showText };
+            });
+        }, 1000);
+    }
+
+    render() {
+        // 根据当前showText的值决定是否显示text内容
+        let display = this.state.showText ? this.props.text : ' ';
+        return (
+            <Text>{display}</Text>
+        );
+    }
+}
+
+export default class BlinkApp extends Component {
+    render() {
+        return (
+            <View>
+                <Blink text='I love to blink' />
+                <Blink text='Yes blinking is so great' />
+                <Blink text='Why did they ever take this out of HTML' />
+                <Blink text='Look at me look at me look at me' />
+            </View>
+        );
+    }
+}
+
+
+
+/**
+ * props实践例子
+ */
+
 class Greeting extends Component {
     render() {
         return (
@@ -10,7 +55,8 @@ class Greeting extends Component {
     }
 }
 
-export default class LotsOfGreetings extends Component {
+
+class LotsOfGreetings extends Component {
     render() {
         return (
             <View style={{alignItems: 'center'}}>
@@ -22,5 +68,9 @@ export default class LotsOfGreetings extends Component {
     }
 }
 
-//JS端注册要暴漏出去的组件 前边引号内的名字需要与我们在Native端声明的要加载到native端的组件名字（2.4步骤中getMainComponentName）相同
-AppRegistry.registerComponent('RNFirstComponent', () => LotsOfGreetings);
+
+/*
+ * JS端注册要暴漏出去的组件 前边引号内的名字需要与我们
+ * 在Native端声明的要加载到native端的组件名字（2.4步骤中getMainComponentName）相同
+ * */
+AppRegistry.registerComponent('RNFirstComponent', () => BlinkApp);
