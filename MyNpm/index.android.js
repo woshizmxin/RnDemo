911f9875cf3de3e8f7d2d8118bca8deb8c2eab67
aimeituan <aimeituan@meituan.com>
2018-07-09 18:32:12 +0800

    实现启动界面；
    Signed-off-by: aimeituan <aimeituan@meituan.com>

--- MyNpm/index.android.js
+++ MyNpm/index.android.js
@@ -1,6 +1,42 @@
-import {AppRegistry, Text, AsyncStorage, ToastAndroid} from "react-native";
-import React, {Component} from 'react'; //导入React的依赖
-import NetTest from "./utils/NetTest";
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+} from 'react-native';
+import {
+    Navigator
+} from 'react-native-deprecated-custom-components'
+import Splash from './views/Splash';
 
+class Index extends Component {
 
-AppRegistry.registerComponent('RNFirstComponent', () => NetTest);
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Navigator
+                initialRoute={{name: '欢迎界面', component: Splash}}
+                configureScene={() => Navigator.SceneConfigs.FloatFromBottom}
+                renderScene={(route, navigator) => {
+                    //返回按钮
+                    navigator.goBack = function () {
+                        navigator.pop();
+                    }
+                    //退出整个app
+                    navigator.exitApp = function () {
+                        let routes = navigator.getCurrentRoutes();
+                        for (var i = 0; i < routes.length; i++) {
+                            navigator.pop();
+                        }
+                    }
+                    return (
+                        <route.component {...route} navigator={navigator}></route.component>
+                    );
+                }}
+            ></Navigator>
+        );
+    }
+}
+
+AppRegistry.registerComponent('RNFirstComponent', () => Index);
---
+++ MyNpm/views/Splash.js
@@ -0,0 +1,69 @@
+import React , { Component } from 'react';
+import {
+    View,
+    Image,
+    BackHandler,
+    Platform,
+} from 'react-native';
+
+import NetTest from '../utils/NetTest'
+
+/**
+ * 启动界面
+ */
+
+let navigator;
+
+export default class Splash extends Component{
+
+    constructor(props){
+        super(props);
+        navigator = this.props.navigator;
+    }
+
+
+    componentWillMount() {
+        if (Platform.OS === 'android') {
+            BackHandler.addEventListener('hardwareBackPress', this.onBackAndroid);
+        }
+    }
+    componentWillUnMount() {
+        if (Platform.OS === 'android') {
+            BackHandler.removeEventListener('hardwareBackPress', this.onBackAndroid);
+        }
+    }
+
+    onBackAndroid() {
+        if (navigator) {
+            let routes = navigator.getCurrentRoutes();
+            if (routes.length > 3) {
+                navigator.pop();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            const navigator = this.props.navigator;
+            if(navigator){
+                navigator.push({
+                    name:'登录界面',
+                    component:NetTest,
+                    params:{
+                        data:'login test',
+                    }
+                });
+            }
+        },2000);
+    }
+
+    render(){
+        return (
+            <View style={{flex:1}}>
+                <Image source={require('../img/hi.jpeg')}></Image>
+            </View>
+        );
+    }
+}
