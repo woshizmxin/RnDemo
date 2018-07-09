6f0815f3f1cbd8dcb57aae937792f2b782581301
aimeituan <aimeituan@meituan.com>
2018-07-09 13:17:28 +0800

    props实践学习；
    Signed-off-by: aimeituan <aimeituan@meituan.com>

--- MyNpm/index.android.js
+++ MyNpm/index.android.js
@@ -1,20 +1,26 @@
 'use strict'; //js文件的生命 使用严格模式
-import React from 'react'; //导入React的依赖
-import { AppRegistry, StyleSheet, Text, View } from 'react-native'; //从react-native包中导入我们需要用的控件，这些控件都是fb官方帮我们在js端封装过的，对应的在native端也已经按照规则暴露并注册过了即在2.4的getPackages()中我们z注册的MainReactPackage
+import React, {Component} from 'react'; //导入React的依赖
+import {AppRegistry, StyleSheet, Text, View} from 'react-native'; //从react-native包中导入我们需要用的控件，这些控件都是fb官方帮我们在js端封装过的，对应的在native端也已经按照规则暴露并注册过了即在2.4的getPackages()中我们z注册的MainReactPackage
 
-//JS端要暴露出去的组件，需要继承自Reactcomponent
-class RNJSFirstComponent extends React.Component {
-    render() { //render函数返回要渲染的布局
+class Greeting extends Component {
+    render() {
         return (
-            <View style={styles.container}>
-            <Text style={styles.textsty}>Hi Jamal Jo</Text>
-            </View> )
-            }
-        }
-//定义的styleSheet 以便布局中引用
-var styles = StyleSheet.create({
-                                container: { flex: 1, justifyContent: 'center', },
-                                textsty: { fontSize: 20, textAlign: 'center', margin: 10, },
-                                    });
+            <Text>Hello {this.props.name}!</Text>
+        );
+    }
+}
+
+export default class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <View style={{alignItems: 'center'}}>
+                <Greeting name='Rexxar'/>
+                <Greeting name='Jaina'/>
+                <Greeting name='Valeera'/>
+            </View>
+        );
+    }
+}
+
 //JS端注册要暴漏出去的组件 前边引号内的名字需要与我们在Native端声明的要加载到native端的组件名字（2.4步骤中getMainComponentName）相同
-AppRegistry.registerComponent('RNFirstComponent', () => RNJSFirstComponent);
+AppRegistry.registerComponent('RNFirstComponent', () => LotsOfGreetings);
